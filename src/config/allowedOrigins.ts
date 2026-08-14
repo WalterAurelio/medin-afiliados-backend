30bd92b10313a2b447d920ffefa4c8dd79ff0779
WalterAurelio <aureliogareca.dev@gmail.com>
2026-08-14 11:03:50 -0300

    feat: add process.env.FRONTEND_URL to allowedOrigins for dynamic origin support

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -1,3 +1,3 @@
-const allowedOrigins = ['http://localhost:3000', 'http://localhost:5173'];
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:5173', process.env.FRONTEND_URL];
 
 export default allowedOrigins;
